947028732f47a953837d9f718ddc3fa409dec28f
ShigeoKogure <ra25888@gmail.com>
2023-11-07 17:21:06 +0900

    fix error log

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -18,13 +17,13 @@ func main() {
 	// Load Env
 	token := os.Getenv("FFLOGS_ACCESS_TOKEN")
 	if token == "" {
-		fmt.Println("FFLOGS_ACCESS_TOKEN is not set in the environment variables")
+		log.Error().Msg("FFLOGS_ACCESS_TOKEN is not set in the environment variables")
 		os.Exit(1)
 	}
 	// Load GraphQL Config
 	config, err := internal.LoadRequestConfig("query.yaml")
 	if err != nil {
-		fmt.Printf("Error loading query config: %s\n", err)
+		log.Error().Stack().Err(err).Msg("Error loading query config")
 		return
 	}
 	fflogsAPICl := internal.FFLogsAPIClient{Client: &http.Client{}}
