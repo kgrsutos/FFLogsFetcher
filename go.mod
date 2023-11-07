a5e29a7e4973d65dd066ca399a0f9d569a492272
ShigeoKogure <ra25888@gmail.com>
2023-11-07 11:38:57 +0900

    use go-graphql-client

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/shigeo-kogure/FFLogsFetcher
 
 go 1.20
 
-require github.com/alecthomas/kong v0.8.1 // indirect
+require (
+	github.com/alecthomas/kong v0.8.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hasura/go-graphql-client v0.10.0 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,100 +1,35 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 
-	"github.com/alecthomas/kong"
+	"github.com/hasura/go-graphql-client"
+	"golang.org/x/oauth2"
 )
 
-// CLIContext はCLIオプションを格納します。
-type CLIContext struct {
-	Query string `arg:"" optional:"" help:"GraphQL query string."`
-}
-
-// GraphQLQuery はGraphQLのクエリを含むリクエストボディを定義します。
-type GraphQLQuery struct {
-	Query string `json:"query"`
-}
-
-// GraphQLResponse はGraphQLのレスポンスを定義します。
-type GraphQLResponse struct {
-	Data struct {
-		ReportData struct {
-			Report struct {
-				Code string `json:"code"`
-			} `json:"report"`
-		} `json:"reportData"`
-	} `json:"data"`
+var query struct {
+	ReportData struct {
+		Report struct {
+			Code string
+		} `graphql:"report(code: $code)"`
+	}
 }
 
 func main() {
-	// 環境変数からトークンを取得
-	token := os.Getenv("FFLOGS_ACCESS_TOKEN")
-	if token == "" {
-		fmt.Println("FFLOGS_ACCESS_TOKEN is not set in the environment variables")
-		os.Exit(1)
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("FFLOGS_ACCESS_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := graphql.NewClient("https://ja.fflogs.com/api/v2/client", httpClient)
+	variables := map[string]interface{}{
+		"code": "Ddk8NJLzmqgQXRnB",
 	}
-
-	// Kongを用いたCLIオプションのパース
-	var cliContext CLIContext
-	kong.Parse(&cliContext)
-
-	// クエリが提供されない場合はデフォルトのクエリを使用
-	if cliContext.Query == "" {
-		cliContext.Query = `{reportData { report(code: "Ddk8NJLzmqgQXRnB") { code } } }`
-	}
-
-	// GraphQLクエリを作成
-	query := GraphQLQuery{
-		Query: cliContext.Query,
-	}
-
-	// JSONに変換
-	queryBody, err := json.Marshal(query)
+	err := client.Query(context.Background(), &query, variables)
 	if err != nil {
-		fmt.Println("Error marshalling query:", err)
-		os.Exit(1)
+		fmt.Println(err.Error())
+		return
 	}
-
-	// リクエストを作成
-	req, err := http.NewRequest("POST", "https://ja.fflogs.com/api/v2/client", bytes.NewBuffer(queryBody))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		os.Exit(1)
-	}
-
-	// リクエストヘッダを設定
-	req.Header.Set("Authorization", "Bearer "+token)
-	req.Header.Set("Content-Type", "application/json")
-
-	// HTTPクライアントを作成し、リクエストを実行
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request to the server:", err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-
-	// レスポンスを読み取る
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		os.Exit(1)
-	}
-
-	// GraphQLレスポンスをデコード
-	var graphqlResponse GraphQLResponse
-	if err := json.Unmarshal(body, &graphqlResponse); err != nil {
-		fmt.Println("Error unmarshalling response body:", err)
-		os.Exit(1)
-	}
-
-	// レスポンスから取得したデータを出力
-	fmt.Println("Code:", graphqlResponse.Data.ReportData.Report.Code)
+	fmt.Println(query.ReportData.Report.Code)
 }
