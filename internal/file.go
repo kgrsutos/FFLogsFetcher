c5a5106d23ddbec11f04a862e8d8bcfc968651d1
ShigeoKogure <ra25888@gmail.com>
2023-11-07 17:00:12 +0900

    change output seq

--- internal/file.go
+++ internal/file.go
@@ -15,7 +15,7 @@ func OutputDeathEvent(path string, events []DeathEventOutput) error {
 	for _, ev := range events {
 		abn := strings.Replace(ev.AbilityName, "{", "", -1)
 		abn = strings.Replace(abn, "}", "", -1)
-		_, err := file.WriteString(fmt.Sprintf("%s\t%v\t%v\n", ev.PlayerName, abn, ev.ReportName))
+		_, err := file.WriteString(fmt.Sprintf("%s\t%v\t%v\n", ev.PlayerName, ev.ReportName, abn))
 		if err != nil {
 			return err
 		}
